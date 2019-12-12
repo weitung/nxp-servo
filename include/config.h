e00a4158f7f2d2ab23956a049d8a71be57b66bb4
wenbin.song <wenbin.song@nxp.com>
2019-12-12 17:42:49 +0800

    nservo base

---
+++ include/config.h
@@ -0,0 +1,15 @@
+/*
+ * config.h
+ *
+ *  Created on: Aug 13, 2019
+ *      Author: songwb
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+#include <stdint.h>
+#include <ecrt.h>
+#include "debug.h"
+#define MAX_SAFE_STACK 0x4000
+#define PDO_ENTRY_NUM_PER_AXLE 140
+#endif /* CONFIG_H_ */
